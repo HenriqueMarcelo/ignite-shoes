c6921408ec172f783fc416b31a25949915b21b49
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-26 14:46:56 -0300

    feat: show how to get action of notification

--- App.tsx
+++ App.tsx
@@ -1,25 +1,48 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native'
+import { NativeBaseProvider } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
-import { Routes } from './src/routes';
+import { Routes } from './src/routes'
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme'
+import { Loading } from './src/components/Loading'
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from './src/contexts/CartContext'
 
-import OneSignal from 'react-native-onesignal';
-import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import OneSignal from 'react-native-onesignal'
+import { tagUserInfoCreate } from './src/notifications/notificationsTags'
 
-
-OneSignal.setAppId('26d1882f-189c-4686-a03d-cca7e5799881');
+OneSignal.setAppId('26d1882f-189c-4686-a03d-cca7e5799881')
 OneSignal.setEmail('marcelo@email.com')
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-  
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   tagUserInfoCreate()
+
+  // useEffect(() => {
+  //   const unsubscribe = OneSignal.setNotificationOpenedHandler((response) => {
+  //     const { actionId } = response.action as any
+
+  //     switch (actionId) {
+  //       case '1':
+  //         console.log('Ver todas')
+  //         break
+  //       case '2':
+  //         console.log('Ver pedido')
+  //         break
+
+  //       default:
+  //         break
+  //     }
+  //   })
+
+  //   return () => unsubscribe
+  // }, [])
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -32,5 +55,5 @@ export default function App() {
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
     </NativeBaseProvider>
-  );
+  )
 }
