cd09316c0f587fb117b7e2a0ee9023d62efb3140
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-26 11:14:05 -0300

    feat: set e-mail for OneSignal

--- App.tsx
+++ App.tsx
@@ -10,7 +10,9 @@ import { Loading } from './src/components/Loading';
 import { CartContextProvider } from './src/contexts/CartContext';
 
 import OneSignal from 'react-native-onesignal';
+
 OneSignal.setAppId('26d1882f-189c-4686-a03d-cca7e5799881');
+OneSignal.setEmail('marcelo@email.com')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
