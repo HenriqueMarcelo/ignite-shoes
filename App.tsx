f177e3f2fe591cb643c970dae7799ed7e5a91556
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-26 13:57:29 -0300

    feat: get notification in foreground

--- App.tsx
+++ App.tsx
@@ -9,8 +9,9 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-import OneSignal from 'react-native-onesignal';
+import OneSignal, { NotificationReceivedEvent } from 'react-native-onesignal';
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
 
 OneSignal.setAppId('26d1882f-189c-4686-a03d-cca7e5799881');
 OneSignal.setEmail('marcelo@email.com')
@@ -19,6 +20,14 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate()
+
+  useEffect(()=> {
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler((notificationRecivedEvent: NotificationReceivedEvent) => {
+      console.log(notificationRecivedEvent)
+    })
+
+    return () => unsubscribe
+  }, []) 
 
   return (
     <NativeBaseProvider theme={THEME}>
