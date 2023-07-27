bccb88e7ea07e3eeb0c6ef565c55200debec7a79
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-27 10:33:48 -0300

    feat: set up linking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,38 +1,61 @@
-import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useTheme } from 'native-base'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 
-import { AppRoutes } from './app.routes';
-import { useEffect, useState } from 'react';
-import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
-import { Notification } from '../components/Notification';
+import { AppRoutes } from './app.routes'
+import { useEffect, useState } from 'react'
+import OneSignal, {
+  NotificationReceivedEvent,
+  OSNotification,
+} from 'react-native-onesignal'
+import { Notification } from '../components/Notification'
+
+const linking = {
+  prefixes: [
+    'igniteshoesapp://',
+    'com.kursi.igniteshoes://',
+    'exp+igniteshoesapp://',
+  ],
+  config: {
+    screens: {
+      details: {
+        path: 'details/:productId',
+        parse: {
+          productId: (productId: string) => productId,
+        },
+      },
+    },
+  },
+}
 
 export function Routes() {
-  const { colors } = useTheme();
+  const { colors } = useTheme()
   const [notification, setNotification] = useState<OSNotification>()
 
-  const theme = DefaultTheme;
-  theme.colors.background = colors.gray[700];
+  const theme = DefaultTheme
+  theme.colors.background = colors.gray[700]
 
-  useEffect(()=> {
-    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler((notificationRecivedEvent: NotificationReceivedEvent) => {
-      const response = notificationRecivedEvent.getNotification()
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationWillShowInForegroundHandler(
+      (notificationRecivedEvent: NotificationReceivedEvent) => {
+        const response = notificationRecivedEvent.getNotification()
 
-      setNotification(response)
-    })
+        setNotification(response)
+      },
+    )
 
     return () => unsubscribe
-  }, []) 
+  }, [])
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
-      {notification?.title && 
+      {notification?.title && (
         <Notification
-          data={notification} 
-          onClose={(() => setNotification(undefined))} 
+          data={notification}
+          onClose={() => setNotification(undefined)}
         />
-      }
+      )}
     </NavigationContainer>
-  );
+  )
 }
