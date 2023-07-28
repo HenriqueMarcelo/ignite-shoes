97925dad920e2eba2d2c9d91acb32544e220ed94
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-07-28 10:54:54 -0300

    feat: set cart into OneSignal

--- src/notifications/notificationsTags.ts
+++ src/notifications/notificationsTags.ts
@@ -1,12 +1,16 @@
-import OneSignal from "react-native-onesignal";
+import OneSignal from 'react-native-onesignal'
 
 export function tagUserEmailCreate(email: string) {
-    OneSignal.sendTag('user_email', email)
+  OneSignal.sendTag('user_email', email)
 }
 
 export function tagUserInfoCreate() {
-    OneSignal.sendTags({
-        user_name: 'Marcelo Henrique',
-        email: 'marcelo.henrique@email.com.br'
-    })
+  OneSignal.sendTags({
+    user_name: 'Henrique Marcelo',
+    email: 'henrique@email.com.br',
+  })
+}
+
+export function tagCartUpdate(itemsCount: string) {
+  OneSignal.sendTag('cart_items_count', itemsCount)
 }
